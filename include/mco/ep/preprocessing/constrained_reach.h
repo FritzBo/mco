bee843543fa5ab5e6581e2d6fb56a33036889fd5
Fritz Bökler <fritz.boekler@tu-dortmund.de>
2014-07-30 20:32:22 +0200

    ep preprocessing

---
+++ include/mco/ep/preprocessing/constrained_reach.h
@@ -0,0 +1,14 @@
+//
+//  constrained_reach.h
+//  mco
+//
+//  Created by Fritz Bökler on 30.07.14.
+//
+//
+
+#ifndef __mco__constrained_reach__
+#define __mco__constrained_reach__
+
+#include <iostream>
+
+#endif /* defined(__mco__constrained_reach__) */
