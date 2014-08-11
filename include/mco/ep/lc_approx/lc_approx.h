19fbcf98433bf994cf2baaea9a51d7a78e09ddc3
Fritz Bökler <fritz.boekler@tu-dortmund.de>
2014-08-11 17:06:06 +0200

    initial lc approx class

---
+++ include/mco/ep/lc_approx/lc_approx.h
@@ -0,0 +1,14 @@
+//
+//  lc_approx.h
+//  mco
+//
+//  Created by Fritz Bökler on 11.08.14.
+//
+//
+
+#ifndef __mco__lc_approx__
+#define __mco__lc_approx__
+
+#include <iostream>
+
+#endif /* defined(__mco__lc_approx__) */
---
+++ mco/ep/lc_approx/lc_approx.cpp
@@ -0,0 +1,9 @@
+//
+//  lc_approx.cpp
+//  mco
+//
+//  Created by Fritz Bökler on 11.08.14.
+//
+//
+
+#include "lc_approx.h"
