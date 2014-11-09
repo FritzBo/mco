e3e3fcf903c3ea22ade82bb274018f4083ffec68
Fritz Bökler <fritz.boekler@tu-dortmund.de>
2014-11-10 00:52:09 +0100

    Better Test of AP benson code.

--- test/ap/dual_benson/ap_hungarian_benson_test.cpp
+++ test/ap/dual_benson/ap_hungarian_benson_test.cpp
@@ -29,12 +29,20 @@ using ogdf::EdgeArray;
 #include <mco/basic/point.h>
 #include <mco/ap/basic/ap_instance.h>
 #include <mco/ap/benson_dual/ap_benson_dual_solver.h>
+#include <mco/generic/benson_dual/ove_cdd.h>
 #include <mco/benchmarks/mcap_parser.h>
+
+#include <mco/ap/brute_force/ap_brute_force_solver.h>
 
 using mco::Point;
 using mco::MCAPParser;
 using mco::AssignmentInstance;
 using mco::APBensonDualSolver;
+using mco::OnlineVertexEnumeratorCDD;
+using mco::EqualityPointComparator;
+
+using mco::APBruteForceSolver;
+
 using mco::EqualityPointComparator;
 
 class ParetoInstanceTestFixture
@@ -55,13 +63,47 @@ TEST_P(ParetoInstanceTestFixture, CountMatch) {
     AssignmentInstance instance = parser.get_instance(graph,
                                                       costs,
                                                       agents);
-    
-    APBensonDualSolver<> solver;
+
+    APBruteForceSolver solver2(instance);
+    solver2.Solve();
+
+//    for(auto s : solver2.solutions()) {
+//        cout << "1\t" << s.second[0];
+//        for(unsigned i = 1; i < instance.dimension(); ++i) {
+//            cout << "\t" << s.second[i];
+//        }
+//        cout << endl;
+//    }
+//
+//    cout << solver2.solutions().size() << endl;
+
+    APBensonDualSolver<OnlineVertexEnumeratorCDD> solver;
     
     solver.Solve(instance);
+
+    cout << solver.number_facets() << endl;
     
     EXPECT_EQ(expected_number_of_minimizers_, solver.solutions().size());
-        
+
+    cout << endl;
+
+    for(auto s : solver.solutions()) {
+        bool found = false;
+        for(auto s2 : solver2.solutions()) {
+            if(EqualityPointComparator(1E-5)(s.second, s2.second)) {
+                found = true;
+                break;
+            }
+        }
+        if(found) {
+            break;
+        } else {
+            cout << "Not found: " << s.second << endl;
+            ASSERT_TRUE(false);
+        }
+        // (26, 90, 75, 100, 99, 78)
+    }
+
     for(auto e : graph.edges) {
         delete costs(e);
     }
@@ -70,6 +112,7 @@ TEST_P(ParetoInstanceTestFixture, CountMatch) {
 INSTANTIATE_TEST_CASE_P(InstanceTests,
                         ParetoInstanceTestFixture,
                         Values(
+                               make_tuple(string("../../../instances/ap/ap_6_8_6"), 295),
                                make_tuple(string("../../../instances/ap/pge_3_10_2"), 28),
                                make_tuple(string("../../../instances/ap/pge_3_05_5"), 12),
                                make_tuple(string("../../../instances/ap/map_1_2_0"), 1)
