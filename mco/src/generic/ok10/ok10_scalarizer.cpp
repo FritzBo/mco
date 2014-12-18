bdaa30a6d3f256c7d4383985dfead256f3347000
Fritz Bökler <fritz.boekler@tu-dortmund.de>
2014-12-18 23:40:53 +0100

    Added a projective version of the ok10 scalarizer.

---
+++ mco/src/generic/ok10/ok10_scalarizer.cpp
@@ -0,0 +1,9 @@
+//
+//  ok10_scalarizer.cpp
+//  mco
+//
+//  Created by Fritz Bökler on 16.12.14.
+//
+//
+
+#include <stdio.h>
---
+++ mco/src/generic/ok10/ok10_scalarizer_projective.cpp
@@ -0,0 +1,9 @@
+//
+//  ok10_scalarizer_projective.cpp
+//  mco
+//
+//  Created by Fritz Bökler on 16.12.14.
+//
+//
+
+#include <stdio.h>
