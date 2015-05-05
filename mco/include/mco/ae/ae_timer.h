441323a21040468008268e6448ac93c0bc2b7cda
Fritz Bökler <fritz.boekler@tu-dortmund.de>
2015-05-05 13:23:10 +0200

    Further additions to ae modules

--- mco/include/mco/ae/ae_timer.h
+++ mco/include/mco/ae/ae_timer.h
@@ -9,6 +9,56 @@
 #ifndef __mco__ae_timer__
 #define __mco__ae_timer__
 
-#include <stdio.h>
+#include <chrono>
+#include <vector>
+#include <ostream>
+
+namespace mco {
+
+class AeTimer {
+public:
+
+    inline void start() {
+        anchor_ = std::chrono::steady_clock::now();
+    }
+
+    inline void stop() {
+        log_time();
+        anchor_ = std::chrono::steady_clock::now();
+    }
+
+    inline void log_time() {
+        using std::chrono::steady_clock;
+        using std::chrono::duration_cast;
+        using std::chrono::duration;
+
+        auto now = steady_clock::now();
+        durations_.push_back(duration_cast<duration<double>>(now - anchor_).count());
+    }
+
+    inline const std::vector<double> & durations() const {
+        return durations_;
+    }
+
+    inline double last() const {
+        return durations_.back();
+    }
+
+    inline double sum() const {
+        double sum = 0;
+        for(auto z : durations_) {
+            sum += z;
+        }
+        return sum;
+    }
+    
+private:
+    std::vector<double> durations_;
+    std::chrono::steady_clock::time_point anchor_;
+};
+
+std::ostream& operator<<(std::ostream& stream, const AeTimer& timer);
+
+}
 
 #endif /* defined(__mco__ae_timer__) */
